e2231b2aec43501a5de789a8d4cf382893484222
Tani <fugdbkfbejdg@gmail.com>
2019-12-24 16:19:06 +0300

    first commit

---
+++ ChatOnline/ChatHub.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatOnline
+{
+    public class ChatHub : Hub
+    {
+        public async Task EnterChat(string username)
+        {
+            if (String.IsNullOrEmpty(username))
+            {
+                await Clients.Caller.SendAsync("NotifyCommonChat", "Для входа в общий чат введите логин");
+            }
+            else
+            {
+                await Clients.All.SendAsync("NotifyCommonChat", $"{username} вошел в общий чат");
+            }
+        }
+        public async Task SendToCommonChat(string message, string userName)
+        {
+            await Clients.All.SendAsync("ReceiveInCommonChat", message, userName);
+        }
+
+
+        public async Task EnterGroup(string username,string groupname)
+        {
+            if (String.IsNullOrEmpty(username)||String.IsNullOrEmpty(groupname))
+            {
+                await Clients.Caller.SendAsync("NotifyGroup", "Для входа в группу введите логин и название группы");
+            }
+            else
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, groupname);
+                await Clients.Group(groupname).SendAsync("NotifyGroup", $"{username} вошел в группу {groupname}");
+            }
+        }
+        public async Task SendToGroup(string message, string userName,string groupName)
+        {
+            await Clients.Group(groupName).SendAsync("ReceiveInGroup", message, userName );
+        }
+       
+    }
+}
---
+++ ChatOnline/wwwroot/js/chat.js
@@ -0,0 +1,75 @@
+﻿"use strict";
+const hubConnection = new signalR.HubConnectionBuilder()
+    .withUrl("/chat")
+    .build();
+
+let userName = '';
+let groupName = '';
+let message = '';
+ //receiving  message from  server to chat
+hubConnection.on('ReceiveInCommonChat', function (message, userName) {
+
+    let userNameElem = document.createElement("b");
+    userNameElem.appendChild(document.createTextNode(userName + ' написал в общем чате : '));
+
+    let elem = document.createElement("p");
+    elem.appendChild(userNameElem);
+    elem.appendChild(document.createTextNode(message));
+
+    var firstElem = document.getElementById("chatroom").firstChild;
+    document.getElementById("chatroom").insertBefore(elem, firstElem);
+
+});
+//receiving  message from  server to group
+hubConnection.on('ReceiveInGroup', function (message, userName) {
+
+    let userNameElem = document.createElement("b");
+    userNameElem.appendChild(document.createTextNode(userName + ' написал в группе : '));
+
+    let elem = document.createElement("p");
+    elem.appendChild(userNameElem);
+    elem.appendChild(document.createTextNode(message ));
+
+    var firstElem = document.getElementById("chatroom").firstChild;
+    document.getElementById("chatroom").insertBefore(elem, firstElem);
+
+});
+
+hubConnection.on("NotifyCommonChat", function (message) {
+
+    let elem = document.createElement("p");
+    elem.appendChild(document.createTextNode(message));
+
+    var firstElem = document.getElementById("chatroom").firstChild;
+    document.getElementById("chatroom").insertBefore(elem, firstElem);
+});
+
+hubConnection.on("NotifyGroup", function (message) {
+
+    let elem = document.createElement("p");
+    elem.appendChild(document.createTextNode(message));
+
+    var firstElem = document.getElementById("chatroom").firstChild;
+    document.getElementById("chatroom").insertBefore(elem, firstElem);
+});
+// enter common chat
+document.getElementById("loginBtn").addEventListener("click", function (e) {
+    userName = document.getElementById("userName").value; //set user
+    hubConnection.invoke("EnterChat", userName);
+});
+
+//  sending message to the server from chat
+document.getElementById("sendBtn").addEventListener("click", function (e) {
+     message = document.getElementById("message").value;//set message
+    hubConnection.invoke("SendToCommonChat", message, userName);
+});
+//enter group
+document.getElementById("groupBtn").addEventListener("click", function (e) {
+    groupName = document.getElementById("groupName").value; //set group
+    hubConnection.invoke("EnterGroup", userName, groupName);
+});
+// sending message to the server from group
+document.getElementById("sendToGroupBtn").addEventListener("click", function (e) {
+    hubConnection.invoke("SendToGroup",message, userName, groupName);
+});
+hubConnection.start();
